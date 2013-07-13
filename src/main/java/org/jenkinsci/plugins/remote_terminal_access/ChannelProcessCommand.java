dd0dbdd9d89d19fb9377dacbc4d07ca5e6fd24f7
Stefan Wolf <stefan.wolf@tngtech.com>
2013-07-13 22:37:23 +0200

    Get correct path of remoting path

--- src/main/java/org/jenkinsci/plugins/remote_terminal_access/ChannelProcessCommand.java
+++ src/main/java/org/jenkinsci/plugins/remote_terminal_access/ChannelProcessCommand.java
@@ -11,6 +11,7 @@ import hudson.model.Label;
 import hudson.model.Queue.Executable;
 import hudson.model.TaskListener;
 import hudson.model.queue.QueueTaskFuture;
+import hudson.remoting.Callable;
 import hudson.remoting.Channel;
 import hudson.remoting.Which;
 import hudson.util.ArgumentListBuilder;
@@ -19,7 +20,6 @@ import org.jenkinsci.plugins.remote_terminal_access.lease.LeasedTask;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +28,7 @@ import java.util.logging.Logger;
 
 /**
  * Launches a new JVM on a slave and connect it with the master with remoting channel.
- *
+ * <p/>
  * stdout/stdin of the client side of the CLI gets connected to the channel, and stderr
  * is forwarded straight to the stderr of the client.
  *
@@ -39,13 +39,13 @@ public class ChannelProcessCommand extends CLICommand {
     /**
      * Label that specifies the slave you'll want.
      */
-    @Argument(required=true)
+    @Argument(required = true)
     public String labelExpr;
 
-    @Option(name="-J",usage="JVM option")
+    @Option(name = "-J", usage = "JVM option")
     public List<String> jvmOpts = new ArrayList<String>();
 
-    @Option(name="-O",usage="Jdk Name option")
+    @Option(name = "-O", usage = "Jdk Name option")
     public String jdkName;
 
     @Override
@@ -60,27 +60,22 @@ public class ChannelProcessCommand extends CLICommand {
         Jenkins.getInstance().checkPermission(TerminalSessionAction.ACCESS);
         Label l = Label.parseExpression(labelExpr);
 
-        LeasedTask t = new LeasedTask(l, Channel.current().getName(),-1,new Runnable() {
+        LeasedTask t = new LeasedTask(l, Channel.current().getName(), -1, new Runnable() {
             public void run() {
                 try {
                     TaskListener taskListener = TaskListener.NULL;  // TODO
                     Computer computer = Computer.currentComputer();
                     EnvVars envVars = getEnvVars(taskListener, computer);
-                    File slaveJar = Which.jarFile(hudson.remoting.Launcher.class);
+                    String slaveJarString = computer.getChannel().call(new GetRemotingJar());
 
                     ArgumentListBuilder args = new ArgumentListBuilder().add("java");
-                    for (String jvmOpt : jvmOpts)
+                    for (String jvmOpt : jvmOpts) {
                         args.add(jvmOpt);
-                    if(slaveJar.isFile()) {
-                        args.add("-jar").add(slaveJar);
                     }
-                    else // in production code this never happens, but during debugging this is convenient
-                    {
-                        args.add("-cp").add(slaveJar).add(hudson.remoting.Launcher.class.getName());
-                    }
+
+                    args.add("-jar").add(slaveJarString);
 
                     Launcher l = computer.getNode().createLauncher(taskListener).decorateByEnv(envVars);
-
 
                     Proc p = l.launch().cmds(args).stdout(stdout).stdin(stdin).stderr(stderr).start();
 
@@ -109,6 +104,14 @@ public class ChannelProcessCommand extends CLICommand {
         f.get();
 
         return exitCode;
+    }
+
+    private static final class GetRemotingJar implements Callable<String, IOException> {
+        private static final long serialVersionUID = 6022357183425911351L;
+
+        public String call() throws IOException {
+            return Which.jarFile(hudson.remoting.Launcher.class).getPath();
+        }
     }
 
     private static final Logger LOGGER = Logger.getLogger(ChannelProcessCommand.class.getName());
